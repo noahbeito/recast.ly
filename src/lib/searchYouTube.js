96f4ae1e0d48920af7887ba4e77f85278b8e4a1f
Noah Beito <noah.beito@gmail.com>
2023-04-11 14:37:46 -0700

    complete searchYouTube function to send a GET request to search youtube api

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -3,9 +3,31 @@ import { API_KEY, YOUTUBE_API_KEY } from '../config/config.js';
 $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
-
+// send a GET request to the search endpoint
+// a callback function is invoked with the videos array that is returned from hitting the endpoint
+// an q string to search for
 var searchYouTube = (query, callback) => {
+
   // TODO
+  $.ajax({
+    type: 'GET',
+    url: 'https://app-hrsei-api.herokuapp.com/api/recastly/videos',
+    data: {
+      key: YOUTUBE_API_KEY,
+      q: query,
+      part: 'snippet',
+      maxResults: 5,
+      type: 'video',
+    },
+    contentType: 'application/json',
+    success: function(data) {
+      data = data.slice(0, 5);
+      callback(data);
+    },
+    error: function(response) {
+      console.log('Search Failed');
+    }
+  });
 };
 
 export default searchYouTube;

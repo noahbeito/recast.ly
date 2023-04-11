cd951e4d83be241d48a536f63e17adae7f26ea9e
Noah Beito <noah.beito@gmail.com>
2023-04-10 20:13:06 -0700

    added state to VideoPlayer to test functionality

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,14 +1,19 @@
-var VideoPlayer = () => (
-  <div className="video-player">
-    <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+const { useState } = React;
+var VideoPlayer = ({video}) => { // pass one video selected by handle
+  const [curVideo, setCurVideo] = useState(video.id.videoId); //videos[0] only play one video
+  const link = 'https://www.youtube.com/embed/' + curVideo;
+  return (
+    <div className="video-player">
+      <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" src={link} allowFullScreen></iframe>
+      </div>
+      <div className="video-player-details">
+        <h3>Video Title</h3>
+        <div>Video Description</div>
+      </div>
     </div>
-    <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
-    </div>
-  </div>
-);
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated

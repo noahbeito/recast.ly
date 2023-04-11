254c87bc9c12ef3658c5d7f5c4c1dc85301da701
Noah Beito <noah.beito@gmail.com>
2023-04-10 20:09:25 -0700

    add VideoPlayer and VideoList components to App in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './Videoplayer.js';
+var App = () => {
+  const videos = exampleVideoData;
+
+  //console.log('App: ', videos);
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em> view goes here</h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em><VideoPlayer video={video}/></em> view goes here</h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><h5><em><VideoList videos={videos}/></em> view goes here</h5></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined

eda6db049ec9ac29580496cfe689fd7396a29fa5
Noah Beito <noah.beito@gmail.com>
2023-04-11 16:16:58 -0700

    add Search component and pass down getVideos function as a prop.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,47 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './Videoplayer.js';
+import searchYouTube from '../lib/searchYouTube.js';
+import Search from './Search.js';
 
 const { useState } = React;
+const { useEffect } = React;
 
 var App = () => {
-  const videos = exampleVideoData;
-  const [curVideo, setCurVideo] = useState(videos[0]); // keep track of the current video in the video list
 
-  const [videoList, setVideoList] = useState(videos);
+  //var videos = exampleVideoData;
+  const [curVideo, setCurVideo] = useState(null);
+  const [videoList, setVideoList] = useState([]);
 
-  var selectVideo = function(videoObj) {
-    setCurVideo(videoObj);
+  const getVideos = (query) => {
+    searchYouTube(query, (data) => {
+      setCurVideo(data[0]);
+      setVideoList(data);
+    });
   };
 
-  // setVideoList([...videoList, { contents: taskString, completed: false }])
-  // const [title, setTitle] = useState(video.snippet.title);
-  // const [description, setDescription] = useState(video.snippet.description);
-  // const [img, setImg] = useState(video.snippet.thumbnails.default.url);
+  useEffect(() => {
+    getVideos('cat');
+  }, []);
+
+
+  var selectVideo = function (videoObj) {
+    setCurVideo(videoObj);
+  };
 
   return (
     <div>
       <nav className="navbar">
         <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
+          <div><h5><em><Search onChange={getVideos} /></em></h5></div>
         </div>
       </nav>
       <div className="row">
         <div className="col-md-7">
-          <div><h5><em><VideoPlayer video={curVideo}/></em> view goes here</h5></div>
+          <div><h5><em><VideoPlayer video={curVideo} /></em></h5></div>
         </div>
         <div className="col-md-5">
-          <div><h5><em><VideoList videos={videoList} handleClick={selectVideo}/></em> view goes here</h5></div>
+          <div><h5><em><VideoList videos={videoList} handleClick={selectVideo} /></em></h5></div>
         </div>
       </div>
     </div>
